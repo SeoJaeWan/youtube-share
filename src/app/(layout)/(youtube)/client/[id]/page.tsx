d4591dcaaf2a78eadea8ade4bd1a5e57bdb3608b
SeoJaeWan <sjw7324@naver.com>
2024-12-18 12:34:55 +0900

    feat: meta data update

--- src/app/(layout)/(youtube)/client/[id]/page.tsx
+++ src/app/(layout)/(youtube)/client/[id]/page.tsx
@@ -1,10 +1,25 @@
 import ClientTemplate from "@/components/templates/client";
-import createMeta from "@/utils/createMeta";
 
-export const metadata = createMeta({
-  title: " - 참가자",
+const client = process.env.NEXT_PUBLIC_CLIENT;
+
+export const metadata = {
+  title: `Rhythm Up - 참가자`,
   description: "초대가 왔어요! 노동요를 함께 만들어보세요.",
-});
+  keywords: "노동요, 재생목록, 유튜브, 노래, 음악, 뮤직",
+  openGraph: {
+    title: `Rhythm Up - 참가자`,
+    description: "초대가 왔어요! 노동요를 함께 만들어보세요.",
+    type: "website",
+    url: client,
+    images: { url: `${client}/assets/common/image.png` },
+  },
+  twitter: {
+    title: `Rhythm Up - 참가자`,
+    description: "초대가 왔어요! 노동요를 함께 만들어보세요.",
+    card: "summary_large_image",
+    images: { url: `${client}/assets/common/image.png` },
+  },
+};
 
 const Client = () => {
   return <ClientTemplate />;
