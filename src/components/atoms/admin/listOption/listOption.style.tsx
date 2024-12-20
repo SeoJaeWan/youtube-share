6b5eca241c7de4e70a09b42e1509d78956a156db
SeoJaeWan <sjw7324@naver.com>
2024-12-20 10:32:09 +0900

    feat: controller icon change

--- src/components/atoms/admin/listOption/listOption.style.tsx
+++ src/components/atoms/admin/listOption/listOption.style.tsx
@@ -5,11 +5,22 @@ interface ContainerProps {
 }
 
 const Container = styled.button<ContainerProps>`
-  opacity: ${(props) => (props.$active ? 1 : 0.6)};
-
-  img {
-    fill: ${(props) => props.theme.color.white};
+  & svg {
+    opacity: ${(props) => (props.$active ? 1 : 0.6)};
   }
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 35px;
+  height: 35px;
+
+  background-color: ${(props) => props.theme.color.primaryBlue};
+  border-radius: 50%;
+
+  padding-left: 0.9px;
+  padding-bottom: 1px;
 `;
 
 const ListOptionStyle = {
